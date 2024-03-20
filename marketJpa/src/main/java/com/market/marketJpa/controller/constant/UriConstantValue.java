eb750445b6a66b7dbdd8585d404a85ccc98bb4af
sanseongKo <tkstjd56@naver.com>
2024-03-20 23:14:09 +0900

    social, password 로그인 uri 변경및 분리

--- marketJpa/src/main/java/com/market/marketJpa/controller/constant/UriConstantValue.java
+++ marketJpa/src/main/java/com/market/marketJpa/controller/constant/UriConstantValue.java
@@ -3,6 +3,7 @@ package com.market.marketJpa.controller.constant;
 public class UriConstantValue {
     public static final String MAILS = "/mails";
     public static final String USERS = "/users";
-    public static final String SIGN_UP = "/sign_up";
+    public static final String SOCIAL_SIGN_UP = "/social_sign_up";
+    public static final String PASSWORD_SIGN_UP = "/password_sign_up";
     public static final String AUTHENTICATION_CODE = "/authentication_code";
 }
