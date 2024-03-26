c50fbdaa07a08a090dcc52c9c5a0db39b1e46b8d
sanseongKo <tkstjd56@naver.com>
2024-03-26 23:21:28 +0900

    user로 passwordLogin 엔티티를 찾을 수 있는 메서드 추가

--- marketJpa/src/main/java/com/market/marketJpa/repository/user/PasswordLoginRepository.java
+++ marketJpa/src/main/java/com/market/marketJpa/repository/user/PasswordLoginRepository.java
@@ -1,7 +1,9 @@
 package com.market.marketJpa.repository.user;
 
 import com.market.marketJpa.vo.user.PasswordLogin;
+import com.market.marketJpa.vo.user.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PasswordLoginRepository extends JpaRepository<PasswordLogin, Long> {
+    PasswordLogin findByUser(Users user);
 }
