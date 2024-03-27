a05df53c8a29e5d630c91674e7838be6c972c941
sanseongKo <tkstjd56@naver.com>
2024-03-27 22:39:27 +0900

    PasswordLoginServiceTest 비밀번호와 유저 정보를 받으면 비밀번호 로그인 정보가 만들어진다 테스트 케이스 추가

---
+++ marketJpa/src/test/java/com/market/marketJpa/service/password/PasswordLoginServiceTest.java
@@ -0,0 +1,54 @@
+package com.market.marketJpa.service.password;
+
+
+import com.market.marketJpa.repository.user.PasswordLoginRepository;
+import com.market.marketJpa.repository.user.UsersRepository;
+import com.market.marketJpa.service.password.generator.PasswordSaltValueGenerator;
+import com.market.marketJpa.vo.user.PasswordLogin;
+import com.market.marketJpa.vo.user.SignUpType;
+import com.market.marketJpa.vo.user.Users;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class PasswordLoginServiceTest {
+
+    @Autowired
+    private PasswordLoginService passwordLoginService;
+    @Autowired
+    private PasswordEncryptClient passwordEncryptClient;
+    @Autowired
+    private PasswordLoginRepository passwordLoginRepository;
+    @Autowired
+    private PasswordSaltValueGenerator generator;
+    @Autowired
+    private UsersRepository usersRepository;
+
+    @DisplayName("비밀번호와 유저 정보를 받으면 비밀번호 로그인 정보가 만들어진다.")
+    @Test
+    void signUpBy() {
+        //given
+        String password = "test_password";
+        String email = "test@email.com";
+
+        Users users = Users.builder()
+                .email(email)
+                .signUpType(SignUpType.SOCIAL)
+                .build();
+
+        Users savedUser = usersRepository.save(users);
+        //when
+        passwordLoginService.signUpBy(password, users);
+        PasswordLogin passwordLogin = passwordLoginRepository.findByUser(users);
+
+        //then
+        assertThat(passwordLogin.getUser().getUserId()).isEqualTo(savedUser.getUserId());
+    }
+}
