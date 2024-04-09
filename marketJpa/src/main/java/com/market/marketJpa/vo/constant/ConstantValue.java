574b358e7488df4808c0e968e7aa7e3ac379f817
sanseongKo <tkstjd56@naver.com>
2024-04-09 23:17:01 +0900

    연관관계 문자열 추가

--- marketJpa/src/main/java/com/market/marketJpa/vo/constant/ConstantValue.java
+++ marketJpa/src/main/java/com/market/marketJpa/vo/constant/ConstantValue.java
@@ -1,7 +1,11 @@
 package com.market.marketJpa.vo.constant;
 
 public class ConstantValue {
-    public static final String USER_PROFILE = "userProfile";
+    public static final String USER_PRODUCT_MAPPER = "userProductMapper";
+
+    public static final String PRODUCT_SEQ = "seq";
+
+    public static final String USER_PROFILE_ID = "user_profile_id";
 
     public static final String USER_ID = "user_id";
 
