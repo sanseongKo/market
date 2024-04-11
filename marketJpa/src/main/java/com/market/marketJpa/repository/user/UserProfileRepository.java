ce1accefc99db41d432543d5133263803a6abe0b
sanseongKo <tkstjd56@naver.com>
2024-04-11 23:09:45 +0900

    커스텀 레파지토리 상속

--- marketJpa/src/main/java/com/market/marketJpa/repository/user/UserProfileRepository.java
+++ marketJpa/src/main/java/com/market/marketJpa/repository/user/UserProfileRepository.java
@@ -3,7 +3,12 @@ package com.market.marketJpa.repository.user;
 import com.market.marketJpa.vo.user.UserProfile;
 import com.market.marketJpa.vo.user.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslPredicate;
 
-public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
+public interface UserProfileRepository
+        extends JpaRepository<UserProfile, Long>,
+        CustomUserProfileRepository,
+        QuerydslPredicateExecutor<UserProfile> {
     UserProfile findByUser(Users user);
 }
